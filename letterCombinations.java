3cc30a52a3f8060c2e6fca845706f6faf1b0216c
정조은 <83575928+joeun-01@users.noreply.github.com>
2022-03-05 01:09:58 +0900

    Add files via upload

---
+++ letterCombinations.java
@@ -0,0 +1,46 @@
+package ¿¬½À¿ë;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+public class letterCombinations {
+	static List<String> list = new ArrayList<>();
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		
+		String digits = keyboard.nextLine();
+		
+		String alphabet[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+		
+		letter(digits, 0, alphabet, "");
+	}
+	public static void letter(String word, int digit, String[] alpha, String output) {
+		int leng = alpha[Integer.parseInt(word.substring(digit, digit + 1))].length();
+		if(digit == word.length()) {
+			list.add(output);
+		}
+		else {
+			int i;
+			for(i = 0; i < leng; i++) {
+				output += alpha[digit].substring(i, i + 1);
+				letter(word, digit + 1, alpha, output);
+				output = output.substring(0, output.length() - 1);
+			}
+		}
+	}
+	/*
+	  private void dfs(int st, String digits, StringBuilder sb, List<String> res, Map<Character, String> map) {
+	        if(st == digits.length()) {
+	            res.add(sb.toString());
+	            return;
+	        }
+	        
+	        String str = map.get(digits.charAt(st));
+	        for(int i = 0; i < str.length(); i++) {
+	            sb.append(str.charAt(i));
+	            dfs(st + 1, digits, sb, res, map);
+	            sb.setLength(sb.length() - 1);
+	        }
+	    }
+	    */
+}
